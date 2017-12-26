b6060dbc717e32379d87bdba7496f32790d3aab8
MINSK\uladzimir_hlushets <Uladzimir_Hlushets@epam.com>
2017-12-26 09:59:17 +0300

    Update Config form

--- src/js/app/services/htmlService.js
+++ src/js/app/services/htmlService.js
@@ -3,73 +3,73 @@ window.htmlService = function() {
     "<div class='row'>" + 
     "<div class='col-md-offset-2 col-md-8'>" +
     "<section id='loginForm'>" +
-        "<form action='/Account/Login' class='form-horizontal' method='post' role='form' novalidate='novalidate'>" +
+        "<form action='http://localhost:63549/api/config' class='form-horizontal' method='post' role='form' novalidate='novalidate'>" +
         "<h4>Configuration.</h4>" +
                 "<hr>" +
                 "<div class='form-group'>" +
-                    "<label class='col-md-2 control-label' for='Email'>IP:</label>" +
+                    "<label class='col-md-2 control-label' for='Ip'>IP:</label>" +
                     "<div class='col-md-10'>" +
-                        "<input class='form-control' data-val='true' data-val-email='The Email field is not a valid e-mail address' data-val-required='The Email field is required.' id='Email' name='Email' type='text'>" +
+                        "<input class='form-control' data-val='true' data-val-required='The Ip field is required.' id='Ip' name='Ip' type='text'>" +
                         "<span class='field-validation-valid text-danger' data-valmsg-replace='true'></span>" +
                     "</div>" +
                 "</div>" +
                 "<div class='form-group'>" +
-                    "<label class='col-md-2 control-label' for='Password'>Port:</label>" +
+                    "<label class='col-md-2 control-label' for='Port'>Port:</label>" +
                     "<div class='col-md-10'>" +
-                        "<input class='form-control' data-val='true' data-val-required='The Password field is required.' id='Password' name='Password' type='text'>" +
-                        "<span class='field-validation-valid text-danger' data-valmsg-for='Password' data-valmsg-replace='true'></span>" +
+                        "<input class='form-control' data-val='true' data-val-required='The Port field is required.' id='Port' name='Port' type='text'>" +
+                        "<span class='field-validation-valid text-danger' data-valmsg-for='Port' data-valmsg-replace='true'></span>" +
                     "</div>" +
                 "</div>" +
                 "<div class='form-group'>" +
-                    "<label class='col-md-2 control-label' for='Password'>Gateway:</label>" +
+                    "<label class='col-md-2 control-label' for='Gateway'>Gateway:</label>" +
                     "<div class='col-md-10'>" +
-                        "<input class='form-control' data-val='true' data-val-required='The Password field is required.' id='Password' name='Password' type='text'>" +
-                        "<span class='field-validation-valid text-danger' data-valmsg-for='Password' data-valmsg-replace='true'></span>" +
+                        "<input class='form-control' data-val='true' data-val-required='The Gateway field is required.' id='Gateway' name='Gateway' type='text'>" +
+                        "<span class='field-validation-valid text-danger' data-valmsg-for='Gateway' data-valmsg-replace='true'></span>" +
                     "</div>" +
                 "</div>" +
                 "<div class='form-group'>" +
-                    "<label class='col-md-2 control-label' for='Password'>Submask:</label>" +
+                    "<label class='col-md-2 control-label' for='Submask'>Submask:</label>" +
                     "<div class='col-md-10'>" +
-                        "<input class='form-control' data-val='true' data-val-required='The Password field is required.' id='Password' name='Password' type='text'>" +
-                        '<span class="field-validation-valid text-danger" data-valmsg-for="Password" data-valmsg-replace="true"></span>' +
+                        "<input class='form-control' data-val='true' data-val-required='The Submask field is required.' id='Submask' name='Submask' type='text'>" +
+                        '<span class="field-validation-valid text-danger" data-valmsg-for="Submask" data-valmsg-replace="true"></span>' +
                     "</div>" +
                 "</div>" +
                 '<div class="form-group">' +
-                    '<label class="col-md-2 control-label" for="Password">DNS:</label>' +
+                    '<label class="col-md-2 control-label" for="Dns">DNS:</label>' +
                     '<div class="col-md-10">' +
-                        '<input class="form-control" data-val="true" data-val-required="The Password field is required." id="Password" name="Password" type="text">' +
-                        '<span class="field-validation-valid text-danger" data-valmsg-for="Password" data-valmsg-replace="true"></span>' +
+                        '<input class="form-control" data-val="true" data-val-required="The Dns field is required." id="Dns" name="Dns" type="text">' +
+                        '<span class="field-validation-valid text-danger" data-valmsg-for="Dns" data-valmsg-replace="true"></span>' +
                     '</div>' +
                 '</div>' +
                 '<div class="form-group">' +
-                    '<label class="col-md-2 control-label" for="Password">DNS 2:</label>' +
+                    '<label class="col-md-2 control-label" for="Dns2">DNS 2:</label>' +
                     '<div class="col-md-10">' +
-                        '<input class="form-control" data-val="true" data-val-required="The Password field is required." id="Password" name="Password" type="text">' +
-                        '<span class="field-validation-valid text-danger" data-valmsg-for="Password" data-valmsg-replace="true"></span>' +
+                        '<input class="form-control" data-val="true" data-val-required="The Dns2 field is required." id="Dns2" name="Dns2" type="text">' +
+                        '<span class="field-validation-valid text-danger" data-valmsg-for="Dns2" data-valmsg-replace="true"></span>' +
                     '</div>' +
                 '</div>' +
                 '<div class="form-group">' +
-                    '<label class="col-md-2 control-label" for="Password">Host:</label>' +
+                    '<label class="col-md-2 control-label" for="Host">Host:</label>' +
                     '<div class="col-md-10">' +
-                        '<input class="form-control" data-val="true" data-val-required="The Password field is required." id="Password" name="Password" type="text">' +
-                        '<span class="field-validation-valid text-danger" data-valmsg-for="Password" data-valmsg-replace="true"></span>' +
+                        '<input class="form-control" data-val="true" data-val-required="The Host field is required." id="Host" name="Host" type="text">' +
+                        '<span class="field-validation-valid text-danger" data-valmsg-for="Host" data-valmsg-replace="true"></span>' +
                     '</div>' +
                 '</div>' +
                  '<div class="form-group">' +
-                    '<label class="col-md-2 control-label" for="Password">DHCP:</label>' +
+                    '<label class="col-md-2 control-label" for="Dhcp">DHCP:</label>' +
                     '<div class="col-md-10">' +
                         '<div class="checkbox">' +
-                            '<input data-val="true" data-val-required="The Remember me? field is required." id="RememberMe" name="RememberMe" type="checkbox" value="true">' +
-                            '<input name="RememberMe" type="hidden" value="false">' +
+                            '<input data-val="true" id="Dhcp" name="Dhcp" type="checkbox" value="true">' +
+                            '<input name="Dhcp" type="hidden" value="false">' +
                         '</div>' +
                     '</div>' +
                 '</div>' +
 
                 '<div class="form-group">' +
-                    '<label class="col-md-2 control-label" for="Password">SSID:</label>' +
+                    '<label class="col-md-2 control-label" for="Ssid">SSID:</label>' +
                     '<div class="col-md-10">' +
-                        '<input class="form-control" data-val="true" data-val-required="The Password field is required." id="Password" name="Password" type="text">' +
-                        '<span class="field-validation-valid text-danger" data-valmsg-for="Password" data-valmsg-replace="true"></span>' +
+                        '<input class="form-control" data-val="true" data-val-required="The Ssid field is required." id="Ssid" name="Ssid" type="text">' +
+                        '<span class="field-validation-valid text-danger" data-valmsg-for="Ssid" data-valmsg-replace="true"></span>' +
                     '</div>' +
                 '</div>' +
                 '<div class="form-group">' +
